1036848b89a9cf264ed1d53375ba1f5860057925
Pawan M <pawanmurthy92@gmail.com>
2018-02-08 02:30:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ function theBeatlesPlay(players, instruments) {
      return allPlayers;
  }
  
- function johnLennonFacts(facts) {
+ function johnLennonFacts() {
+    var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
     var newArr=[];
     var i=0;
     while(i<facts.length){
