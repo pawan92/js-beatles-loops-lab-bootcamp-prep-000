ce2bc52ff88efaa5208aecd1b5d77c0753da7764
Pawan M <pawanmurthy92@gmail.com>
2018-02-08 02:15:13 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,17 @@
+function theBeatlesPlay(players, instruments) {
+     var allPlayers = [];
+     for (var i=0; i < players.length; i++) {
+         allPlayers.push(players[i] + " plays " + instruments[i]);
+     }
+     return allPlayers;
+ }
+ 
+ function johnLennonFacts(facts) {
+    var newArr=[];
+    i=0
+    while(i<facts.length){
+      newArr.push(newArr[i] +"!!!");
+         i++;
+     }
+    
+ }
