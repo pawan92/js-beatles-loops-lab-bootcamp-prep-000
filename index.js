749e5e863705783d006c4be8dcaa072bd6f3274a
Pawan M <pawanmurthy92@gmail.com>
2018-02-08 02:39:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,15 +6,15 @@ function theBeatlesPlay(players, instruments) {
      return allPlayers;
  }
  
- function johnLennonFacts() {
-    var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
-    var newArr=[];
-    var i=0;
-    while(i<facts.length){
-      newArr.push(facts[i] +"!!!");
+function johnLennonFacts() {
+     var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
+     var newFacts = [];
+     var i = 0;
+     while (i < facts.length) {
+         newFacts.push(facts[i] +"!!!");
          i++;
      }
-    
+     return newFacts;
  }
  
  function iLoveTheBeatles(num) {
